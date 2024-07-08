dcfb53119f8664f7f1861f4bbcd33a74cb2941b4
Victor <victor.s.viana@outlook.com>
2024-07-08 17:16:48 -0300

    Create a database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000 ☕ > http://localhost:3000");
+  console.log("Servidor rodando na porta 3000 ☕ > http://localhost:3000");
 });
