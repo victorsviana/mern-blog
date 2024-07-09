638771138f246a43c292cb9e9b30aa482840be07
Victor <victor.s.viana@outlook.com>
2024-07-09 12:14:43 -0300

    Add functionality to the signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,50 @@
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, seterrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.username || !formData.email || !formData.password) {
+      return seterrorMessage("Todos os campos devem ser preenchidos!");
+    }
+
+    try {
+      setLoading(true);
+      seterrorMessage(null);
+      const res = await fetch("api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      
+      const data = await res.json();
+
+      if (data.success === false) {
+        setLoading(false);
+        return seterrorMessage(data.message);
+      }
+      setLoading(false);
+
+      if (res.ok) {
+        navigate("/signin");
+      }
+    } catch (error) {
+      seterrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -9,7 +52,7 @@ export default function SignUp() {
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              M.E.R.N.
+              Gangue
             </span>
             Blog
           </Link>
@@ -22,27 +65,60 @@ export default function SignUp() {
 
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="">
               <Label value="Nome de Usuário" />
-              <TextInput type="text" placeholder="Usuário" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Usuário"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Email" />
-              <TextInput type="text" placeholder="nome@email.com" id="email" />
+              <TextInput
+                type="email"
+                placeholder="nome@email.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Senha" />
-              <TextInput type="text" placeholder="Senha" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Senha"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Entrar
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-2">Carregando...</span>
+                </>
+              ) : (
+                "Cadastrar"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Ja tem uma conta?</span>
-            <Link className="text-blue-500 font-semibold" to="/signin">Entrar</Link>
+            <Link className="text-blue-500 font-semibold" to="/signin">
+              Entrar
+            </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
